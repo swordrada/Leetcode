fd6496d2a01ee2a6f091a4d74d72fb19e6934c73
root <root@A006449-NC01.ESG.360ES.CN>
2019-02-17 20:19:57 +0800

    001-T_Sum.py First Commit

--- 001-Two_Sum.py
+++ 001-Two_Sum.py
@@ -8,7 +8,8 @@ class Solution:
         dict = {}
         for i,num in enumerate(nums):
             # 'enumerate(nums)' is like [(0,2),(1,7),(2,11),(3,15)] 
-            if target-num in dict:                                                                                          return [dict[target-num], i]
+            if target-num in dict:
+                return [dict[target-num], i]
             else:
                 dict[num] = i
             # If not in dict,add it.'dict' is empty at first.
